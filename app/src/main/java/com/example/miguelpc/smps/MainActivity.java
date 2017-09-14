ff36db879d5f0aeee22e599239f818be6317bf1f
Miguelangel Palma <mangel.2711@gmail.com>
2017-09-14 17:05:03 -0400

    Creando actividades de stepdown y stepup

--- app/src/main/java/com/example/miguelpc/smps/MainActivity.java
+++ app/src/main/java/com/example/miguelpc/smps/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.miguelpc.smps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvStepDown;
+    TextView tvStepUp;
+    ImageView imgA;
+    ImageView imgB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgA = (ImageView)findViewById(R.id.imagen_a);
+        imgB = (ImageView)findViewById(R.id.imagen_b);
+        tvStepDown = (TextView)findViewById(R.id.tvStep_down);
+        tvStepUp = (TextView)findViewById(R.id.tvStep_up);
+
+        imgA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),StepUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        imgB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),StepDownActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
