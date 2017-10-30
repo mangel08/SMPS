ba4133eedfb650c899c424530eed8e695f563025
Miguelangel Palma <mangel.2711@gmail.com>
2017-10-29 21:42:48 -0400

    nuevos cambios en Operacion Astable

--- app/src/main/java/com/example/miguelpc/smps/OpEstableActivity.java
+++ app/src/main/java/com/example/miguelpc/smps/OpEstableActivity.java
@@ -21,11 +21,11 @@ import java.text.DecimalFormat;
 public class OpEstableActivity extends AppCompatActivity {
 
     private static final String TAG = OpEstableActivity.class.getSimpleName();
-    public String v = "";
+    public String v;
     public String _C3;
-    public EditText etCt, etRb, etC3;
+    public EditText etFrecuencia, etRA, etC3;
     public Button btnCalcular;
-    public TextView tvR1, tvRB, tvRA, tvC1, tvC2, tvC3, tvD1, tvR4, tvR5, tvC6, tvU3, tvFrecuencia, tvElemento;
+    public TextView tvR1, tvRB, tvRA, tvC1, tvC2, tvC3, tvD1, tvR4, tvR5, tvC6, tvU3, tvFrecuencia, tvElemento, tvCT;
     public LinearLayout linearLayout1;
     public FloatingActionButton fab;
     public double R1 = 1000;
@@ -35,7 +35,7 @@ public class OpEstableActivity extends AppCompatActivity {
     public double frecuencia = 0;
     public double C1 = 45;
     public double C2 = 0.1;
-    public double CT;
+    public double CT, Periodo, F;
     public String D1 = "D1: Diodo de alta frecuencia";
     public String R4 = "R4: 100 Ω";
     public String R5 = "R5: 2000 Ω";
@@ -52,9 +52,11 @@ public class OpEstableActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        v = getIntent().getStringExtra("view");
+
         //EditText del Fomulario
-        etCt = (EditText) findViewById(R.id.ciclo_trabajo);
-        etRb = (EditText) findViewById(R.id.rb);
+        etFrecuencia = (EditText) findViewById(R.id.frecuencia);
+        etRA = (EditText) findViewById(R.id.ra);
         etC3 = (EditText) findViewById(R.id.c3);
 
         //LinearLayout
@@ -73,6 +75,7 @@ public class OpEstableActivity extends AppCompatActivity {
         tvR5 = (TextView)findViewById(R.id.tvR5);
         tvC6 = (TextView)findViewById(R.id.tvC6);
         tvU3 = (TextView)findViewById(R.id.tvU3);
+        tvCT = (TextView)findViewById(R.id.tvCT);
 
         tvElemento = (TextView)findViewById(R.id.tvElemento);
         tvFrecuencia = (TextView)findViewById(R.id.tvFrecuencia);
@@ -87,49 +90,60 @@ public class OpEstableActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                String aux_ct = etCt.getText().toString();
-                String aux_rb = etRb.getText().toString();
+                String aux_frecuencia = etFrecuencia.getText().toString();
+                String aux_ra = etRA.getText().toString();
+                ra = Double.parseDouble(aux_ra);
                 String aux_c3 =  etC3.getText().toString();
 
-                if(!aux_ct.equals("") &&  !aux_rb.equals("") && !aux_c3.equals("")){
+                if(!aux_frecuencia.equals("") &&  !aux_ra.equals("") && !aux_c3.equals("")){
 
+                    aux_c3 = String.valueOf(Double.parseDouble(aux_c3));
 
+                    //Valor original de C3
+                    _C3 = aux_c3;
 
-                    if(Double.parseDouble(aux_ct) <= 0 || Double.parseDouble(aux_ct) >= 1 ){
+                    //Valor C3 parseado y dividi entre 1Millon
+                    C3 = Double.parseDouble(aux_c3) / 1000000L;
 
-                        Toast.makeText(getApplicationContext(), "Ciclo de trabajo debe ser un valor mayor a 0 y menor a 1 (0.1 ... 0.9)", Toast.LENGTH_LONG).show();
+                    //Valor de la Frecuencia obtenida por el usuario
+                    F = Double.parseDouble(aux_frecuencia);
 
+                    Periodo = Periodo(F);
 
+                    rb = CalcularRB(Periodo,Double.parseDouble(aux_ra),C3);
+                    CT = CicloTrabajo(Double.parseDouble(aux_ra), rb);
+
+                    if(rb < 0){
+                        Toast.makeText(getApplicationContext(), "Por favor ajuste el valor de C3 a un valor menor", Toast.LENGTH_LONG).show();
                     }else{
-
-                        rb = Double.parseDouble(aux_rb);
-                        Log.e(TAG, "C3: " + String.valueOf(aux_c3));
-                        aux_c3 = String.valueOf(Double.parseDouble(aux_c3));
-                        Log.e(TAG, "C3: " + String.valueOf(aux_c3));
-                        C3 = Double.parseDouble(aux_c3) / 1000000L;
-                        Log.e(TAG, "C3: " + String.valueOf(C3));
-                        _C3 = aux_c3;
-
-                        CT = Double.parseDouble(aux_ct);
-
-                        ra = CalcularRa(Double.parseDouble(aux_rb), Double.parseDouble(aux_ct));
-                        frecuencia = CalcularFrecuencia(ra,Double.parseDouble(aux_rb), C3);
-
-                        tvR1.setText("R1: " + df2.format(R1)+" Ω");
-                        tvRA.setText("RA: "+ df2.format(ra) + " Ω");
-
-                        tvRB.setText("RB: " + aux_rb + " Ω");
-                        tvC1.setText("C1: " + C1 + " μF");
-                        tvC2.setText("C2: " + C2 + " μF");
-                        tvC3.setText("C3: " + aux_c3 + " μF");
-                        tvD1.setText(D1);
-                        tvR4.setText(R4);
-                        tvR5.setText(R5);
-                        tvC6.setText(C6);
-                        tvU3.setText(U3);
-                        tvElemento.setText("Elemento integrado: " + Elemento);
-                        tvFrecuencia.setText("Frecuencia: " + df2.format(frecuencia) + " Hz");
-
+                        if(v.equals("SU")){
+                            Toast.makeText(getApplicationContext(), "SU", Toast.LENGTH_LONG).show();
+                            tvR1.setText("R1: " + df2.format(R1)+" Ω");
+                            tvRA.setText("RA: "+ df2.format(ra) + " Ω");
+                            tvRB.setText("RB: " + df.format(rb)+ " Ω");
+                            tvC1.setText("C1: " + C1 + " μF");
+                            tvC2.setText("C2: " + C2 + " μF");
+                            tvC3.setText("C3: " + aux_c3 + " μF");
+                            tvElemento.setText("Elemento integrado: " + Elemento);
+                            tvFrecuencia.setText("Frecuencia: " + df2.format(F) + " Hz");
+                            tvCT.setText("Ciclo de trabajo: " + df2.format(CT));
+                        }else{
+                            Toast.makeText(getApplicationContext(), "SD", Toast.LENGTH_LONG).show();
+                            tvR1.setText("R1: " + df2.format(R1)+" Ω");
+                            tvRA.setText("RA: "+ df2.format(ra) + " Ω");
+                            tvRB.setText("RB: " + df2.format(rb)+ " Ω");
+                            tvC1.setText("C1: " + C1 + " μF");
+                            tvC2.setText("C2: " + C2 + " μF");
+                            tvC3.setText("C3: " + aux_c3 + " μF");
+                            tvElemento.setText("Elemento integrado: " + Elemento);
+                            tvFrecuencia.setText("Frecuencia: " + df2.format(F) + " Hz");
+                            tvCT.setText("Ciclo de trabajo: " + df.format(CT));
+                            tvD1.setText(D1);
+                            tvR4.setText(R4);
+                            tvR5.setText(R5);
+                            tvC6.setText(C6);
+                            tvU3.setText(U3);
+                        }
                         linearLayout1.setVisibility(View.VISIBLE);
                         fab.setVisibility(View.VISIBLE);
                     }
@@ -147,9 +161,9 @@ public class OpEstableActivity extends AppCompatActivity {
                 Intent i = new Intent(getApplicationContext(), PWMActivity.class);
                 i.putExtra("view", v);
                 i.putExtra("R1", df2.format(R1));
+                i.putExtra("C", C6);
                 i.putExtra("R4", R4);
                 i.putExtra("R5", R5);
-                i.putExtra("C", C6);
                 i.putExtra("U3", U3);
                 i.putExtra("D1", D1);
                 i.putExtra("RA", df2.format(ra));
@@ -165,31 +179,76 @@ public class OpEstableActivity extends AppCompatActivity {
         });
     }
 
-    public Double CalcularFrecuencia(double ra, double rb, double c3){
 
-        Log.e(TAG, "RA: " + String.valueOf(ra));
-        Log.e(TAG, "RB: " + String.valueOf(rb));
-        Log.e(TAG, "C3: " + String.valueOf(c3));
 
-        double result = 1.44/((ra+2*rb)*c3);
-        Log.e(TAG, "FRECUENCIA: " + String.valueOf(result));
+    public Double Periodo(double frecuencia){
 
-        result = result * 1000;
+        Log.d(TAG, "F: " + String.valueOf(frecuencia));
 
-        Log.e(TAG, "FRECUENCIA: " + String.valueOf(result));
+        double result = 1/frecuencia;
+
+        Log.e(TAG, "periodo: " + String.valueOf(result));
+        Log.d(TAG, " ");
 
         return result;
     }
 
-    public Double CalcularRa(double rb, double ct){
+    public Double CalcularRB(double periodo, double RA, double C3){
 
-        Log.e(TAG, "RB: " + String.valueOf(rb));
-        Log.e(TAG, "CT: " + String.valueOf(ct));
+        Log.e(TAG, "Periodo: " + String.valueOf(periodo));
+        Log.e(TAG, "RA: " + String.valueOf(RA));
+        Log.e(TAG, "C3: " + String.valueOf(C3));
 
-        double result = (rb/(1-ct))-2*rb;
+        double result = (periodo - (RA*C3*0.693))/(2*C3*0.693);
 
-        Log.e(TAG, "RA: " + String.valueOf(result));
+        Log.e(TAG, "RB: " + String.valueOf(result));
+        Log.e(TAG, " ");
 
         return result;
+
     }
+
+    public Double CicloTrabajo(double RA, double RB){
+
+        Log.d(TAG, "RA: " + String.valueOf(RA));
+        Log.d(TAG, "RB: " + String.valueOf(RB));
+
+        double result = 1-(RB/((2*RB)+RA));
+
+        Log.e(TAG, "CT: " + String.valueOf(result));
+        Log.d(TAG, " ");
+
+        return  result;
+    }
+
+//    public Double CalcularRa(double rb, double ct){
+//
+//        Log.d(TAG, "RB: " + String.valueOf(rb));
+//        Log.d(TAG, "CT: " + String.valueOf(ct));
+//
+//        double result = (rb/(1-ct))-2*rb;
+//
+//        Log.e(TAG, "RA: " + String.valueOf(result));
+//        Log.d(TAG, " ");
+//
+//        return result;
+//    }
+
+
+//    public Double CalcularFrecuencia(double ra, double rb, double c3){
+//
+//        Log.e(TAG, "RA: " + String.valueOf(ra));
+//        Log.e(TAG, "RB: " + String.valueOf(rb));
+//        Log.e(TAG, "C3: " + String.valueOf(c3));
+//
+//        double result = 1.44/((ra+2*rb)*c3);
+//        Log.e(TAG, "FRECUENCIA: " + String.valueOf(result));
+//
+//        result = result * 1000;
+//
+//        Log.e(TAG, "FRECUENCIA: " + String.valueOf(result));
+//
+//        return result;
+//    }
+
 }
